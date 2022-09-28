dd91d726ac30178524e8b396405a522c886c8158
gRoussac <gRoussac@users.noreply.github.com>
2022-09-28 17:37:05 +0200

    update api

--- apps/evaluator-backend/src/app/app.service.ts
+++ apps/evaluator-backend/src/app/app.service.ts
@@ -1,8 +1,39 @@
 import { Injectable } from '@nestjs/common';
+import { JSDOM } from 'jsdom';
+import * as jQuery from 'jquery';
+const window = new JSDOM('').window;
+const document = window.document;
+const jquery = jQuery(window);
 
 @Injectable()
 export class AppService {
-  getData(): { message: string } {
-    return { message: 'Welcome to evaluator-backend!' };
+  getData(): { [key: string]: string[]; } {
+    const windowObj = Object.getOwnPropertyNames(global).filter((p: any) => {
+      return typeof global[p] === 'function' && !p.startsWith('_');
+    });
+    const stringObj = Object.getOwnPropertyNames(String.prototype).filter((p: string) => {
+      return typeof String.prototype[p] === 'function';
+    });
+    Object.getOwnPropertyNames(String).filter((p: string) => {
+      return typeof String[p] === 'function';
+    }).forEach((func) => {
+      stringObj.push(func);
+    });
+    const documentObj = Object.getOwnPropertyNames(Object.getPrototypeOf(document)).filter((p: string) => {
+      return typeof document[p] === 'function';
+    });
+    const jsonObj = Object.getOwnPropertyNames(JSON).filter((p: string) => {
+      return typeof JSON[p] === 'function';
+    });
+    const jqueryObj = Object.getOwnPropertyNames(jquery).filter((p: string) => {
+      return typeof jquery[p] === 'function';
+    });
+    return {
+      'window': windowObj,
+      'String': stringObj,
+      'document': documentObj,
+      'json': jsonObj,
+      'jquery': jqueryObj
+    };
   }
 }
