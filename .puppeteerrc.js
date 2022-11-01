fabfe79e32c93177386fbb0bb632e7b9d96502ab
gRoussac <gRoussac@users.noreply.github.com>
2022-11-01 17:08:53 +0100

    module

--- .puppeteerrc.js
+++ .puppeteerrc.js
@@ -1,6 +1,8 @@
-import { join } from 'path';
+const { join } = require('path');
 
 /**
  * @type {import("puppeteer").Configuration}
  */
-export const cacheDirectory = join(__dirname, '.cache', 'puppeteer');
+module.exports = {
+  cacheDirectory: join(__dirname, '.cache', 'puppeteer'),
+};
