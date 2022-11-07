5383bb72d97f3d43424027d2985ee86943078d0d
gRoussac <gRoussac@users.noreply.github.com>
2022-11-07 23:58:50 +0100

    Update main.rs

--- evaluator/src/main.rs
+++ evaluator/src/main.rs
@@ -3,16 +3,10 @@ use async_process::Command;
 use std::env;
 
 #[derive(Debug)]
-struct Evaluator {
-    pub inputs: Vec<String>,
-}
+struct Evaluator {}
 
 impl Evaluator {
-    fn new() -> Self {
-        Self { inputs: Vec::new() }
-    }
-
-    fn set_inputs(&mut self, file: &String, func: &String) {
+    async fn set_inputs(&mut self, file: &String, func: &String) {
         let file = std::fs::File::open(file).unwrap();
         let mut rdr = csv::ReaderBuilder::new()
             .has_headers(true)
@@ -22,40 +16,37 @@ impl Evaluator {
             match result {
                 Ok(res) => {
                     // println!("{:?}", res.get(0).unwrap());
-                    let site = res.get(0).unwrap();
-                    self.fetch(&site.to_string(), func);
+                    let site = res.get(0).unwrap().to_string();
+                    let site = &format!("http://{}", site);
+                    println!("site : {}", site);
+                    let output = Command::new("node")
+                        .arg("pupet.js")
+                        .arg(site)
+                        .arg(func)
+                        .output()
+                        .await
+                        .expect("failed to execute process");
+                    let stdout = &output.stdout;
+                    if !stdout.is_empty() {
+                        println!("{}", String::from_utf8_lossy(stdout));
+                    }
                 }
                 Err(e) => println!("Error {:?}", e),
             }
         }
     }
-
-    fn fetch(&self, site: &String, func: &String) {
-        let site = &format!("http://{}", site);
-        println!("site : {}", site);
-        let output = Command::new("node")
-            .arg("pupet.js")
-            .arg(site)
-            .arg(func)
-            .output()
-            .expect("failed to execute process");
-        let stdout = &output.stdout;
-        if !stdout.is_empty() {
-            // println!("site : {}", site);
-            println!("{}", String::from_utf8_lossy(stdout));
-        }
-    }
 }
 
-fn main() {
+#[async_std::main]
+async fn main() {
     let args: Vec<String> = env::args().collect();
-    run(&args);
+    run(&args).await;
 }
 
-fn run(args: &[String]) {
+async fn run(args: &[String]) {
     let file = args.get(1).unwrap();
     let func = args.get(2).unwrap();
     println!("In file {}", file);
-    let mut evaluator = Evaluator::new();
-    evaluator.set_inputs(file, func);
+    let mut evaluator: Evaluator = Evaluator {};
+    evaluator.set_inputs(file, func).await;
 }
